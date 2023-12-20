38d7cfbf8fc8e1ad7c637cc7d6dacb7b4da59aff
Gabriel Marques <59850744+G40b@users.noreply.github.com>
2023-12-20 10:10:15 -0300

    Create Task Entity

---
+++ src/main/java/com/gabriel/springtodolist/task/ITaskRepository.java
@@ -0,0 +1,9 @@
+package com.gabriel.springtodolist.task;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ITaskRepository extends JpaRepository<TaskModel, UUID> {
+
+}
