ecbe64a74810fd2e33a66c4339bd3cdadd4c34b7
Gabriel Marques <59850744+G40b@users.noreply.github.com>
2023-12-20 22:44:48 -0300

    Set request attribute

--- src/main/java/com/gabriel/springtodolist/filter/FilterAuth.java
+++ src/main/java/com/gabriel/springtodolist/filter/FilterAuth.java
@@ -49,6 +49,7 @@ public class FilterAuth extends OncePerRequestFilter {
                 var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
 
                 if (passwordVerify.verified) {
+                    request.setAttribute("idUser", user.getId());
                     filterChain.doFilter(request, response);
 
                 } else {
