e867222865099ac1d70ee6b163b2f1d3ccef35f0
Gabriel Marques <59850744+G40b@users.noreply.github.com>
2023-12-20 23:14:46 -0300

    List tasks

--- src/main/java/com/gabriel/springtodolist/task/TaskController.java
+++ src/main/java/com/gabriel/springtodolist/task/TaskController.java
@@ -1,11 +1,13 @@
 package com.gabriel.springtodolist.task;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -42,4 +44,11 @@ public class TaskController {
 
         return ResponseEntity.status(HttpStatus.OK).body(task);
     };
+
+    @GetMapping
+    public List<TaskModel> list(HttpServletRequest request) {
+        var idUser = request.getAttribute("idUser");
+
+        return iTaskRepository.findByIdUser((UUID) idUser);
+    }
 }
